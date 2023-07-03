1bac7bff8edd4d59c1f4f09338070ccc9905cd9f
Jose Berrio <aldarisbm@gmail.com>
2023-07-03 09:20:01 -0400

    getting table name from row

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 import os
 import xml.etree.ElementTree as ET
-from typing import List
+from typing import List, Generator
+from xml.etree.ElementTree import Element
+
 from load_xml import PostgresConnection
 
 
 def main():
     with PostgresConnection() as pg:
-        for row in analyze_xml_files():
-            if row.tag == "row":
-                columns = []
-                values = []
-                for attr_name, attr_value in row.attrib.items():
-                    columns.append(attr_name)
-                    values.append(attr_value)
+        for iterator in analyze_xml_files('parenting.meta.stackexchange.com'):
+            for row in iterator:
+                if row.tag != "row":
+                    print(row.tag)
 
-                columns = [column.lower() for column in columns]
-                values = [value.replace("'", "''") for value in values]
-                values = [f"'{value}'" for value in values]
-                pg.load_row_into_table("votes", columns, values)
+                if row.tag == "row":
+                    columns = []
+                    values = []
+                    for attr_name, attr_value in row.attrib.items():
+                        columns.append(attr_name)
+                        values.append(attr_value)
 
-def analyze_xml_files():
-    dir_path = 'parenting.meta.stackexchange.com'
+                    columns = [column.lower() for column in columns]
+                    values = [value.replace("'", "''") for value in values]
+                    values = [f"'{value}'" for value in values]
+                    print(f'Columns: {columns}, Values: {values}')
+                    # pg.load_row_into_table("votes", columns, values)
+
+
+def analyze_xml_files(dir_path: str) -> list[Generator[Element, None, None]]:
     file_names = get_files_in_dir(dir_path)
+    iterators = []
     for file in file_names:
-        if file == "Votes.xml":
-            return xml_analyze(f'{dir_path}/{file}')
+        iterators.append(xml_analyze(f'{dir_path}/{file}'))
+    return iterators
+
 
 def get_files_in_dir(dir_path: str) -> List[str]:
     files = []
